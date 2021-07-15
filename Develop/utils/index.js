9a94db74b35b3b3df71bbc1703d71f2986f22427
PN-Barnes <paulieb99@gmail.com>
2021-07-15 10:15:36 -0700

    included the folder generate markdown in Index.html to call on functions found in markdown file

--- Develop/utils/index.js
+++ Develop/utils/index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('generateMarkdown')
 // TODO: Create an array of questions for user input
 const questions = [];
 inquirer
