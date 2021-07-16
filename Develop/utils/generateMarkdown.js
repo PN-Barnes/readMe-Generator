9347e493952d1fc712e03e8cf6887d4e6a7ea709
PN-Barnes <paulieb99@gmail.com>
2021-07-16 10:13:18 -0700

    created the render badge function to test and called render badge and render link within render section and called that within license section of generate markdown

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -2,46 +2,51 @@
 // If there is no license, return an empty string
 var licenseBadge;
 var licenseImg;
-// function renderLicenseBadge(license) {
-//   switch (license) {
-//     case 'MIT':
-//        license = "![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)" 
+function renderLicenseBadge(license) {
+  switch (license) {
+    case 'MIT':
+       license = "![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)" 
       
-//       break;
+      break;
 
-//     case 'Mozilla':
-//       licenseBadge = 
-//       "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)"
+    case 'Mozilla':
+      licenseBadge = 
+      "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)"
       
-//       break;
+      break;
 
-//     case 'Apache':
-//       licenseBadge = 
+    case 'Apache':
+      licenseBadge = 
       
-//       "![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)"
+      "![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)"
       
-//       break;
+      break;
+    case 'BSD-2':
+      license =
+      '![License](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg)'
 
-//     case 'BSD-3':
-//       licenseBadge = 
-//       "![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)"
-//       break;
+      break;
 
-//     case 'Unlicense':
-//       licenseBadge = 
-//        "![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)"
-//       break;
-//     case 'Boost':
-//       licenseBadge = 
-//       "![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)"
-//       
-//       break;
+    case 'BSD-3':
+      licenseBadge = 
+      "![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)"
+      break;
+
+    case 'Unlicense':
+      licenseBadge = 
+       "![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)"
+      break;
+    case 'Boost':
+      licenseBadge = 
+      "![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)"
+      
+      break;
 
 
-//     default:
-//       break;
-//   }
-// }
+    default:
+      break;
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -65,6 +70,11 @@ function renderLicenseLink(license) {
       "[License: Apache-2.0](https://opensource.org/licenses/Apache-2.0)"
       
       break;
+    case 'BSD-2':
+      license = 
+      '[License](https://opensource.org/licenses/BSD-2-Clause)'
+
+      break;
 
     case 'BSD-3':
       license = 
@@ -77,6 +87,7 @@ function renderLicenseLink(license) {
       "[License: Unlicense](http://unlicense.org/)"
       
       break;
+
     case 'Boost':
       license = 
       "[License](https://www.boost.org/LICENSE_1_0.txt)"
@@ -85,6 +96,7 @@ function renderLicenseLink(license) {
 
 
     default:
+      license = ''
       break;
   }
   return license;
@@ -92,7 +104,10 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  renderLicenseBadge(data.license);
+  renderLicenseLink(data.license);
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -122,7 +137,7 @@ function generateMarkdown(data) {
   
   ## License 
   
-  ${renderLicenseLink(data.license)}
+  ${renderLicenseSection(data.license)}
 
   ## Contributing
 
